cf77e49319eb781e7ddf96b182c806e567f947f3
assad30 <sattari.assad30@gmail.com>
2026-08-30 04:07:34 +0500

    Faild code added

--- src/main/java/com/learning/jenkins/api/SpringJenkinsApplication.java
+++ src/main/java/com/learning/jenkins/api/SpringJenkinsApplication.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
-	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	publicc static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	@PostConstruct
 	public void init(){
