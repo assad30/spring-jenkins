fac55619825667cc5fd0c41ea4a98948f43671dc
assad30 <sattari.assad30@gmail.com>
2026-09-06 02:06:17 +0500

    Added Error

--- src/test/java/com/learning/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/learning/jenkins/api/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	public svoid contextLoads() {
 		logger.info("Test case execution started ..");
 		Assertions.assertTrue(true);
 	}
